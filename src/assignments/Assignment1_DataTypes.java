83df1f3159617b70bf6960f358c5547697359651
Sushant Patil <sush.patil02@gmail.com>
2025-03-01 17:33:05 +0530

    Assignment 1 Data_Type

--- src/assignments/Assignment1_DataTypes.java
+++ src/assignments/Assignment1_DataTypes.java
@@ -3,8 +3,199 @@ package assignments;
 public class Assignment1_DataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 1. Temperature of the city in degree Celsius: 25.5
 
+		float temperature = 25.5f; 
+		
+		System.out.println(" Temperature of the city in degree Celsius" +25.5);
+		
+		// 2. Whether a customer has place an order: true or false
+		
+		boolean customerhasplaceanorder = true;
+		
+		
+		boolean customernotplaceanorder = false;
+		
+
+		System.out.println(" if Customer has place an order will be shown as " +customerhasplaceanorder);
+	
+		System.out.println(" if Customer has not place an order it should populate as "   +customernotplaceanorder);
+	
+	
+		// 3. Person's phone number: "123-456-7890"
+		
+		int  phonenumber = 123456789;
+		
+		System.out.println(" Phone number of sushant is " + phonenumber );
+		
+		
+		// 4.  Amount of money in a customer's bank account: 1000.50 
+		
+		
+		float Savings = 100.50f;
+		
+		System.out.println( "Amount of money in a customer's bank account is " + Savings  );
+		
+		
+		// 5. Person's email address: "john.doe@example.com" 
+		
+		String Johnemailaddress = "john.doe@example.com ";
+		
+		System.out.println(" Email address of John is " + Johnemailaddress );
+		
+		 
+		// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194 
+		
+		float lattitue = 37.7749f;
+		System.out.println(" Lattitude of the state is equal to " + lattitue);
+		
+	
+        float longitude = -122.4194f;	
+	
+        System.out.println(" longitude of the state is equal to " +longitude  );
+        
+        
+        // 7. Person's marital status: true or false 
+        
+        boolean a = true;
+        
+        boolean b = false;
+        
+        System.out.println(" IF person is married =" + a );
+        
+        System.out.println(" IF person is unmarried =" + b );
+        
+        
+        // 8. Person's occupation: "Software Engineer" 
+        
+        
+        String x = " Person's Occupation = Software Engineer ";
+        
+        System.out.println( x );
+        
+        
+        
+        // 9 .  Person's favourite colour: "Blue" 
+         
+        String  c = " Person's favourite colour = Blue ";
+        
+        System.out.println(c );
+        
+        
+        // 10. Current year: 2025 
+        
+        short m = 2025;
+        
+        System.out.println( " Current year is " +m);
+        
+        
+       // 11.Number of followers on a social media platform: 1,000,000 
+        
+        
+        int y = 1000000;
+        
+        System.out.println(" Number of followers on a social media platform:" +y);
+    
+        // 12 . .Rating of a movie: 7.5 
+        
+        float Movie = 7.5f;
+        
+        System.out.println(" Rating of a movie, out of 10 is " + Movie);
+        
+        
+        // 13 . .Person's blood type: 'A' 
+        
+        char bloodtype = 'A';
+        
+        System.out.println(" Blood type of the person is  "+ bloodtype );
+        
+        
+        
+        // 14 . Title of a book: "To Kill a Mockingbird"
+        
+        
+        String booktitle = "To Kill a Mockingbird";
+        
+        System.out.println(" Title of a book : " +booktitle);
+        
+        
+        // 15. Number of employees in a company: 500 
+        
+        short employee = 500;
+        
+        System.out.println(" Number of employee is a company:" +employee );
+        
+        
+        // 16. Time of an event: 2:30 PM 
+        
+        
+        float Time = 2.30f;
+        
+        System.out.println(" Time of an event is " +Time);
+        
+        
+        
+        // 17 . Name of a country: "United States" 
+        
+        String country = "United States";
+        
+        System.out.println(" Name of a country:" +country );
+        
+        
+        // 18 . Person's eye color: "Brown" 
+        
+        String Eye = "Brown";
+        
+        System.out.println(" Person's eye color:" +Eye );
+        
+        
+        // 19 . Person's birthplace: "New York City" 
+        
+        
+        String birthplace = "New York City";
+        
+        System.out.println(" Person's birthplace:" + birthplace);
+        
+        
+        // 20. .Distance between two cities: 200.5 
+        
+        float Distance = 200.5f;
+        
+        System.out.println("Distance between two cities in Km is:" + Distance);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 	}
+	
+	
+	
 
 }
+
+
+
